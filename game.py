bb42405a499b1d3208e43bcd765f25d747f8780a
ymin86kim <ymin86kim@gmail.com>
2024-06-13 14:33:12 +0900

    [refactoring] 66

--- game.py
+++ game.py
@@ -18,18 +18,27 @@ class Game:
 
     def guess(self, guessNumber):
         self.assert_illegal_value(guessNumber)
-        if guessNumber == self.question:
-            return GameResult(True, 3, 0)
-        else:
-            strikes = 0
-            balls = 0
-            for i in range(len(self.question)):
-                if self.question.find(guessNumber[i]) == i:
-                    strikes += 1
-                elif self.question.find(guessNumber[i]) > -1:
-                    balls += 1
 
-            return GameResult(False, strikes, balls)
+        if self.is_solved(guessNumber):
+            return self.get_success_game_result()
+        else:
+            return self.get_unsolved_game_result(guessNumber)
+
+    def get_unsolved_game_result(self, guessNumber):
+        strikes = 0
+        balls = 0
+        for i in range(len(self.question)):
+            if self.question.find(guessNumber[i]) == i:
+                strikes += 1
+            elif self.question.find(guessNumber[i]) > -1:
+                balls += 1
+        return GameResult(False, strikes, balls)
+
+    def get_success_game_result(self):
+        return GameResult(True, 3, 0)
+
+    def is_solved(self, guessNumber):
+        return guessNumber == self.question
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
